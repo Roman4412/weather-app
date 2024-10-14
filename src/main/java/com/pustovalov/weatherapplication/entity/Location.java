a63ee89edc8dbec16f9dbba2899a6486c7f6ad0f
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2024-10-14 11:47:46 +0300

    feature: add Location entity

---
+++ src/main/java/com/pustovalov/weatherapplication/entity/Location.java
@@ -0,0 +1,36 @@
+package com.pustovalov.weatherapplication.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name = "locations")
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 30)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "latitude", nullable = false)
+    private BigDecimal latitude;
+
+    @Column(name = "longitude", nullable = false)
+    private BigDecimal longitude;
+
+}
