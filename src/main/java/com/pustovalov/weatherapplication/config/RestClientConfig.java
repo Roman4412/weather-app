3c87fcf7a7e070af5eeae680ccbc2d0bfcbfa09b
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2024-10-15 09:20:35 +0300

    feat: add the configuration for the RestClient

---
+++ src/main/java/com/pustovalov/weatherapplication/config/RestClientConfig.java
@@ -0,0 +1,25 @@
+package com.pustovalov.weatherapplication.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
+
+@Configuration
+public class RestClientConfig {
+
+    @Value("${api.base-url}")
+    private String url;
+
+    @Bean
+    public RestClient restClient() {
+        ClientHttpRequestFactorySettings settings = ClientHttpRequestFactorySettings.DEFAULTS.withConnectTimeout(
+                Duration.ofSeconds(3)).withReadTimeout(Duration.ofSeconds(5));
+
+        return RestClient.builder().requestFactory(ClientHttpRequestFactories.get(settings)).baseUrl(url).build();
+    }
+}
