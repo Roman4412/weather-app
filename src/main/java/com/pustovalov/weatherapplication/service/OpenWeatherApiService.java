9275034198e1428905ae60ef2fc3678bb95d8d66
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2024-10-15 15:16:39 +0300

    feat: change the methods for working with the api
      - now getWeather is looking for the weather in the target location
      - added a method to search for suitable locations by name

--- src/main/java/com/pustovalov/weatherapplication/service/OpenWeatherApiService.java
+++ src/main/java/com/pustovalov/weatherapplication/service/OpenWeatherApiService.java
@@ -1,16 +1,23 @@
 package com.pustovalov.weatherapplication.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import com.pustovalov.weatherapplication.dto.WeatherApiGeoResponse;
+import com.pustovalov.weatherapplication.dto.response.WeatherData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
-import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
 
 @RequiredArgsConstructor
 
 @Service
 public class OpenWeatherApiService {
+
 
     private static final String Q_PARAM_NAME = "q";
 
@@ -24,34 +31,52 @@ public class OpenWeatherApiService {
 
     private static final String DEFAULT_UNITS = "metric";
 
+    private static final String LIMIT_PARAM_NAME = "limit";
+
+    private static final String GEO_PATH = "/geo/1.0/direct";
+
+    private static final String WEATHER_PATH = "/data/2.5/weather";
+
     private final RestClient restClient;
+
+    private final ObjectMapper objectMapper;
 
     @Value("${api.key}")
     private String apiKey;
 
-    public void getWeather(String cityName) {
+    public List<WeatherApiGeoResponse> getLocations(String cityName) {
         //TODO обработать ошибки api
-        restClient.get()
-                .uri(uriBuilder -> uriBuilder.path("/data/2.5/weather")
+        //TODO нужны повторные попытки соединения при неудачной попытке
+        List<WeatherApiGeoResponse> result;
+
+        String body = restClient.get().uri(uriBuilder -> uriBuilder.path(GEO_PATH)
                         .queryParam(Q_PARAM_NAME, cityName)
+                        .queryParam(LIMIT_PARAM_NAME, 15)
                         .queryParam(APPID_PARAM_NAME, apiKey)
-                        .queryParam(UNITS_PARAM_NAME, DEFAULT_UNITS)
                         .build())
                 .retrieve()
                 .body(String.class);
+
+        CollectionType targetType = objectMapper.getTypeFactory().constructCollectionType(List.class,
+                                                                                          WeatherApiGeoResponse.class);
+
+        try {
+            result = Collections.unmodifiableList(objectMapper.readValue(body, targetType));
+        } catch (JsonProcessingException e) {
+            result = Collections.emptyList();
+            e.printStackTrace();
+        }
+        return result;
     }
 
-    public void getWeather(BigDecimal latitude, BigDecimal longitude) {
+    public WeatherData getWeather(Location location) {
         //TODO обработать ошибки api
-        restClient.get()
-                .uri(uriBuilder -> uriBuilder.path("/data/2.5/weather")
-                        .queryParam(LATITUDE_PARAM_NAME, latitude)
-                        .queryParam(LONGITUDE_PARAM_NAME, longitude)
+        //TODO нужны повторные попытки соединения при неудачной попытке
+        return restClient.get().uri(uriBuilder -> uriBuilder.path(WEATHER_PATH)
+                .queryParam(LATITUDE_PARAM_NAME, location.getLatitude())
+                .queryParam(LONGITUDE_PARAM_NAME, location.getLongitude())
                         .queryParam(APPID_PARAM_NAME, apiKey)
                         .queryParam(UNITS_PARAM_NAME, DEFAULT_UNITS)
-                        .build())
-                .retrieve()
-                .body(String.class);
+                        .build()).retrieve().body(WeatherData.class);
     }
-
 }
